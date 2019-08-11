e3c52df47a208168a14d583a3891b055fbf04f32
mrxenysoff <mrxenysoff@gmail.com>
2019-08-11 11:56:22 +0700

    secondly

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-
+from .forms import UserCreationForm
 
 class HomePageView(TemplateView):
     template_name = 'accounts/home.html'
@@ -12,6 +12,9 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')  # перенаправляет в случае успешного логирования
     template_name = 'accounts/signup.html'
+
+
+
 
 class AboutView(TemplateView):
     template_name = 'accounts/about.html'
--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fq4t1b7sow@6=6ivw3htm79$(k81)xu_7ku_4%njd-&&upkf4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
