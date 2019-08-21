dee813148cde839f41bb61803b1a68ca6f693cda
mrxenysoff <mrxenysoff@gmail.com>
2019-08-21 07:26:11 +0000

    second

--- myproject/settings.py
+++ myproject/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/home/mrxenysoff/project/static')
-#STATICFILES_DIRS = (
-     #os.path.join(BASE_DIR, 'static'),
-#)
+
 
 
 
