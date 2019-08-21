99d45fd89ce8d4f1dde2102ff0b37105cee848ba
mrxenysoff <mrxenysoff@gmail.com>
2019-08-21 07:04:17 +0000

    second

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open('/home/mrxenysoff/secret_keys/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
